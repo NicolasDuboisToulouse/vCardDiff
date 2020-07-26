84508a3f71532b77f1245d527e1e6e9532ab013d
Nicolas Dubois <DuboisNicolasToulouse@users.noreply.github.com>
2020-07-26 09:39:31 +0200

    Allow several fields with same key

--- src/vcard.hh
+++ src/vcard.hh
@@ -12,7 +12,7 @@ namespace vcard {
     typedef std::string                      key_t;
     typedef std::string                      value_t;
     typedef std::pair<const key_t, value_t>  field_t;
-    typedef std::map<key_t, value_t>         fields_t;
+    typedef std::multimap<key_t, value_t>    fields_t;
 
     inline void clear() { _fields.clear(); }
     inline void insert(const key_t& key, const value_t& value) { _fields.insert(field_t(key, value)); }
